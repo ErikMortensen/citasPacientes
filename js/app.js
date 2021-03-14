8bc84248710e36295e33faf04750d35c57b12572
Erik Mortensen <mortensenerikm@gmail.com>
2021-03-13 21:37:57 -0300

    Lee el formulario y carga el objeto cita

---
+++ js/app.js
@@ -0,0 +1,38 @@
+// Campos del usuario
+const mascotaInput = document.querySelector('#mascota');
+const propietarioInput = document.querySelector('#propietario');
+const telefonoInput = document.querySelector('#telefono');
+const fechaInput = document.querySelector('#fecha');
+const horaInput = document.querySelector('#hora');
+const sintomasInput = document.querySelector('#sintomas');
+
+const formulario = document.querySelector('#nueva-cita');
+const contenedorCitas = document.querySelector('#citas');
+
+// Registrar Eventos
+eventListeners();
+function eventListeners(){
+    mascotaInput.addEventListener('input', datosCita);
+    propietarioInput.addEventListener('input', datosCita);
+    telefonoInput.addEventListener('input', datosCita);
+    fechaInput.addEventListener('input', datosCita);
+    horaInput.addEventListener('input', datosCita);
+    sintomasInput.addEventListener('input', datosCita);
+}
+
+// Objeto con la información de la cita
+const citaObj = {
+    mascota: '',
+    propietario: '',
+    telefono: '',
+    fecha: '',
+    hora: '',
+    sintomas: '',
+}
+
+// Agrega datos al Obj de cita
+function datosCita(e){
+    // Para que funcione hay que definir un name con el mismo nombre que las propiedades del Obj
+    citaObj[e.target.name] = e.target.value;
+    console.log(citaObj);
+}
